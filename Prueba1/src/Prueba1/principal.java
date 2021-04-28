d3f2bcf7f336437fa38437e958ab5eea2fff5f8c
Rui <Rui@LAPTOP-M02SB4N3>
2021-04-28 09:05:31 +0200

    Cambio en la clase principal

--- Prueba1/src/Prueba1/principal.java
+++ Prueba1/src/Prueba1/principal.java
@@ -3,5 +3,9 @@ package Prueba1;
 public class principal {
 	public static void main(String[] args) {
 		System.out.print("Hola Mundo Java-GIT");
+		System.out.print("Hola Mundo Java-GIT");
+		System.out.print("Hola Mundo Java-GIT");
+		System.out.print("Hola Mundo Java-GIT");
+		System.out.print("Hola Mundo Pruebas GIT cambio 1");
 	}
 }
